b50679c861a383b064b78432c6ad3a619ef7e93c
USER <USER@DESKTOP-0PKE0U9>
2023-05-26 11:47:31 +0900

    박소리

--- ItsDone/src/Sori_GitTest.java
+++ ItsDone/src/Sori_GitTest.java
@@ -3,6 +3,7 @@ public class Sori_GitTest {
 
 	public static void main(String[] args) {
 		System.out.println("저희 강아지 이름은 보리에요");
+		System.out.println("mung mumg");
 	    System.out.println("시고르자브종");
 
 	}
