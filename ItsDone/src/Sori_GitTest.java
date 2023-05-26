b6df6e97b10cce73b86d8f6c5be27a8a06ae4445
USER <USER@DESKTOP-0PKE0U9>
2023-05-26 11:45:19 +0900

    Merge branch 'master' of https://github.com/2023-SMHRD-SW-DataDesign-1/ItsDone.git 박소리

--- ItsDone/src/Sori_GitTest.java
+++ ItsDone/src/Sori_GitTest.java
@@ -2,7 +2,8 @@
 public class Sori_GitTest {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("저희 강아지 이름은 보리에요");
+	    System.out.println("시고르자브종");
 
 	}
 
