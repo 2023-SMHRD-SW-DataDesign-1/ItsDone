9982589c9d7fb9c269eef22b6e9b9ebd8e3be6b7
smhrd <smhrd@DESKTOP-1JO9FEG>
2023-05-31 09:13:11 +0900

    2023 0531 황운기 view단추가2

--- BlueMarble/src/view/MainMenu.java
+++ BlueMarble/src/view/MainMenu.java
@@ -161,35 +161,5 @@ public class MainMenu {
       }
    }
 
-   public static void boardpan(String[] args) {
-
-      System.out.println("+----------------------------------------------------------------------------------+");
-      System.out.println("|  몬트리올 |  황금열쇠  |  상파울로  |  시드니   |  하와이   |  리스폰   |   마드리드    |  우주여행 |");
-      System.out.println("|  200000 |         |  240000 | 240000  |  260000 |  260000 |   260000   |         |");
-      System.out.println("|---------|--------------------------------------------------------------|---------|");
-      System.out.println("|  황금열쇠  |                                                              |   도쿄   |");
-      System.out.println("|         |                                                              |  300000 |");
-      System.out.println("|---------|                                                              |---------|");
-      System.out.println("|  아테네   |                                                              |   파리   |");
-      System.out.println("|  180000 |                                                              |  320000 |");
-      System.out.println("|---------|                                                              |---------|");
-      System.out.println("|  스톡홀름  |                                                              | 황금열쇠  |");
-      System.out.println("|  160000 |                                                              |         |");
-      System.out.println("|---------|                                                              |---------|");
-      System.out.println("|  코펜하겐  |                                                              |   런던   |");
-      System.out.println("|  160000 |                                                              |  350000 |");
-      System.out.println("|---------|                                                              |---------|");
-      System.out.println("|  황금열쇠  |                                                              |   뉴욕   |");
-      System.out.println("|         |                                                              |  350000 |");
-      System.out.println("|---------|                                                              |---------|");
-      System.out.println("|  아테네   |                                                              |   서울   |");
-      System.out.println("|  140000 |                                                              |  500000 |");
-      System.out.println("|---------|--------------------------------------------------------------|---------|");
-      System.out.println("|  무인도   |  이스탄불  |   카이로   |  황금열쇠  |  싱가포르  |  마닐라   |  타이페이  |   출발    |");
-      System.out.println("|         | 120000   | 100000  |          | 100000  |  80000  |  50000  |          |");
-      System.out.println("+----------------------------------------------------------------------------------+");
-      
-      
-         
-   }
+   
 }
---
+++ BlueMarble/src/view/boardpan.java
@@ -0,0 +1,159 @@
+package view;
+
+public class boardpan {
+
+	public static void main(String[] args) {
+
+        
+        System.out.printf("%-6s", "+--------");
+        System.out.printf("%-6s", "---------");
+        System.out.printf("%-6s", "---------");
+        System.out.printf("%-6s", "---------");
+        System.out.printf("%-6s", "---------");
+        System.out.printf("%-6s", "---------");
+        System.out.printf("%-6s", "---------");
+        System.out.printf("%-6s%n", "---------------+");
+       System.out.printf("|%-6s|", "몬트리올");
+       System.out.printf("|%-6s|", "황금열쇠");
+       System.out.printf("|%-6s|", "상파울로");
+       System.out.printf("|%-6s|", "시드니");
+       System.out.printf("|%-7s|", "하와이");
+       System.out.printf("|%-6s|", "리스본");
+       System.out.printf("|%-6s|", "마드리드");
+       System.out.printf("|%-5s|%n", "우주여행");
+       System.out.printf("|%-8d|", 200000);
+       System.out.printf("|%-8s|", "*");
+       System.out.printf("|%-8d|", 240000);
+       System.out.printf("|%-8d|", 240000);
+       System.out.printf("|%-8d|", 260000);
+       System.out.printf("|%-8d|", 260000);
+       System.out.printf("|%-8d|", 260000);
+       System.out.printf("|%-7s|%n", "***");
+       System.out.printf("%-6s", "|--------");
+        System.out.printf("%-6s", "---------");
+        System.out.printf("%-6s", "---------");
+        System.out.printf("%-6s", "---------");
+        System.out.printf("%-6s", "---------");
+        System.out.printf("%-6s", "---------");
+        System.out.printf("%-6s", "---------");
+        System.out.printf("%-6s%n", "---------------|");
+        System.out.printf("|%-6s|", "황금열쇠");
+        System.out.printf("%-60s", " ");
+        System.out.printf("|%-7s|%n", "도쿄");
+        System.out.printf("|%-8s|", "*");
+        System.out.printf("%-60s", " ");
+        System.out.printf("|%-8d|%n", 300000);
+        System.out.printf("|%-6s|", "--------");
+        System.out.printf("%-60s", " ");
+        System.out.printf("|%-6s|%n", "--------");
+        System.out.printf("|%-6s|", "베를린");
+        System.out.printf("%-60s", " ");
+        System.out.printf("|%-7s|%n", "파리");
+        System.out.printf("|%-8d|", 180000);
+        System.out.printf("%-60s", " ");
+        System.out.printf("|%-8d|%n", 320000);
+        System.out.printf("|%-6s|", "--------");
+        System.out.printf("%-60s", " ");
+        System.out.printf("|%-6s|%n", "--------");
+        System.out.printf("|%-6s|", "스톡홀름");
+        System.out.printf("%-60s", " ");
+        System.out.printf("|%-6s|%n", "황금열쇠");
+        System.out.printf("|%-8d|", 160000);
+        System.out.printf("%-60s", " ");
+        System.out.printf("|%-8s|%n", "*");
+        System.out.printf("|%-6s|", "--------");
+        System.out.printf("%-60s", " ");
+        System.out.printf("|%-6s|%n", "--------");
+        System.out.printf("|%-6s|", "코펜하겐");
+        System.out.printf("%-60s", " ");
+        System.out.printf("|%-7s|%n", "런던");
+        System.out.printf("|%-8d|", 160000);
+        System.out.printf("%-60s", " ");
+        System.out.printf("|%-8d|%n", 350000);
+        System.out.printf("|%-6s|", "--------");
+        System.out.printf("%-60s", " ");
+        System.out.printf("|%-6s|%n", "--------");
+        System.out.printf("|%-6s|", "황금열쇠");
+        System.out.printf("%-60s", " ");
+        System.out.printf("|%-7s|%n", "뉴욕");
+        System.out.printf("|%-8s|", "*");
+        System.out.printf("%-60s", " ");
+        System.out.printf("|%-8d|%n", 350000);
+        System.out.printf("|%-6s|", "--------");
+        System.out.printf("%-60s", " ");
+        System.out.printf("|%-6s|%n", "--------");
+        System.out.printf("|%-6s|", "아테네");
+        System.out.printf("%-60s", " ");
+        System.out.printf("|%-7s|%n", "서울");
+        System.out.printf("|%-8d|", 140000);
+        System.out.printf("%-60s", " ");
+        System.out.printf("|%-8d|%n", 500000);
+        System.out.printf("%-6s", "|--------");
+        System.out.printf("%-6s", "---------");
+        System.out.printf("%-6s", "---------");
+        System.out.printf("%-6s", "---------");
+        System.out.printf("%-6s", "---------");
+        System.out.printf("%-6s", "---------");
+        System.out.printf("%-6s", "---------");
+        System.out.printf("%-6s%n", "----------------|");
+       System.out.printf("|%-6s|", "무인도");
+       System.out.printf("|%-6s|", "이스탄불");
+       System.out.printf("|%-7s|", "카이로");
+       System.out.printf("|%-6s|", "황금열쇠");
+       System.out.printf("|%-6s|", "싱가포르");
+       System.out.printf("|%-6s|", "마닐라");
+       System.out.printf("|%-6s|", "타이페이");
+       System.out.printf("|%-8s|%n", "출발");
+       System.out.printf("|%-8s|", "-*");
+       System.out.printf("|%-8d|", 120000);
+       System.out.printf("|%-8d|", 100000);
+       System.out.printf("|%-8s|", "*");
+       System.out.printf("|%-8d|", 100000);
+       System.out.printf("|%-8d|", 80000);
+       System.out.printf("|%-8d|", 50000);
+       System.out.printf("|%-8s|%n", "<-");
+       System.out.printf("%-6s", "+--------");
+        System.out.printf("%-6s", "---------");
+        System.out.printf("%-6s", "---------");
+        System.out.printf("%-6s", "---------");
+        System.out.printf("%-6s", "---------");
+        System.out.printf("%-6s", "---------");
+        System.out.printf("%-6s", "---------");
+        System.out.printf("%-6s%n", "----------------+");
+        
+
+
+	}
+//	public static void boardpan(String[] args) {
+//
+//	      System.out.println("+----------------------------------------------------------------------------------+");
+//	      System.out.println("|  몬트리올 |  황금열쇠  |  상파울로  |  시드니   |  하와이   |  리스폰   |   마드리드    |  우주여행 |");
+//	      System.out.println("|  200000 |         |  240000 | 240000  |  260000 |  260000 |   260000   |         |");
+//	      System.out.println("|---------|--------------------------------------------------------------|---------|");
+//	      System.out.println("|  황금열쇠  |                                                              |   도쿄   |");
+//	      System.out.println("|         |                                                              |  300000 |");
+//	      System.out.println("|---------|                                                              |---------|");
+//	      System.out.println("|  아테네   |                                                              |   파리   |");
+//	      System.out.println("|  180000 |                                                              |  320000 |");
+//	      System.out.println("|---------|                                                              |---------|");
+//	      System.out.println("|  스톡홀름  |                                                              | 황금열쇠  |");
+//	      System.out.println("|  160000 |                                                              |         |");
+//	      System.out.println("|---------|                                                              |---------|");
+//	      System.out.println("|  코펜하겐  |                                                              |   런던   |");
+//	      System.out.println("|  160000 |                                                              |  350000 |");
+//	      System.out.println("|---------|                                                              |---------|");
+//	      System.out.println("|  황금열쇠  |                                                              |   뉴욕   |");
+//	      System.out.println("|         |                                                              |  350000 |");
+//	      System.out.println("|---------|                                                              |---------|");
+//	      System.out.println("|  아테네   |                                                              |   서울   |");
+//	      System.out.println("|  140000 |                                                              |  500000 |");
+//	      System.out.println("|---------|--------------------------------------------------------------|---------|");
+//	      System.out.println("|  무인도   |  이스탄불  |   카이로   |  황금열쇠  |  싱가포르  |  마닐라   |  타이페이  |   출발    |");
+//	      System.out.println("|         | 120000   | 100000  |          | 100000  |  80000  |  50000  |  ★      |");
+//	      System.out.println("+----------------------------------------------------------------------------------+");
+//	      
+//	      
+//	         
+//	   }
+
+}
