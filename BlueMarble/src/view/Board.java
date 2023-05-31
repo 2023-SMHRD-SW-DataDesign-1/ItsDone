a81b92df6de37fd4776bed8234cf26e29c248757
smhrd <smhrd@DESKTOP-LAE6013>
2023-05-31 16:00:13 +0900

    230531 윤명석 player.jar 추가

--- BlueMarble/src/view/Board.java
+++ BlueMarble/src/view/Board.java
@@ -15,6 +15,8 @@ public class Board {
 	private static GameController gc = new GameController();
 
 	public static void main(String[] args) {
+		
+		System.out.println("anjdi");
 
 		Scanner scan = new Scanner(System.in);
 
