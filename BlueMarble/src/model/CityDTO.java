c2b1d23c9a0425ad3655e4d66fb1e333fd8e2ecb
smhrd <smhrd@DESKTOP-LAE6013>
2023-05-31 09:05:01 +0900

    230531 윤명석 mvc 수정

--- BlueMarble/src/model/CityDTO.java
+++ BlueMarble/src/model/CityDTO.java
@@ -1,30 +1,112 @@
 package model;
 
 public class CityDTO {
+	
+	private String name;
+	private int owner = -1;
+	private int price;
+	private int house_price;
+	private int building_price;
+	private int hotel_price;
+	private boolean house;
+	private boolean building;
+	private boolean hotel;
+	
+	public int getHouse_price() {
+		return house_price;
+	}
 
-	//도시
-	private int atoll; //통행료
-	private int price; //건물가격
-	
-	private int summerhouse; //별장
-	private int building; //빌딩
-	private int hotel; //호텔
-	private int landmark; //랜드마크
-	private int tourism; //관광지
-	
-	public CityDTO(int atoll, int price, int summerhouse, int building, int hotel, int landmark, int tourism) {
-		super();
-		this.atoll = atoll;
-		this.price = price;
-		this.summerhouse = summerhouse;
-		this.building = building;
-		this.hotel = hotel;
-		this.landmark = landmark;
-		this.tourism = tourism;
+
+	public void setHouse_price(int house_price) {
+		this.house_price = house_price;
+	}
+
+
+	public int getBuilding_price() {
+		return building_price;
+	}
+
+
+	public void setBuilding_price(int building_price) {
+		this.building_price = building_price;
+	}
+
+
+	public int getHotel_price() {
+		return hotel_price;
+	}
+
+
+	public void setHotel_price(int hotel_price) {
+		this.hotel_price = hotel_price;
 	}
 	
 	
+	public CityDTO(String name, int price) {
+		super();
+		this.name = name;
+		this.price = price;
+	}
+
+
+	public String getName() {
+		return name;
+	}
+
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+
+	public int getOwner() {
+		return owner;
+	}
+
+
+	public void setOwner(int owner) {
+		this.owner = owner;
+	}
+
+
+	public int getPrice() {
+		return price;
+	}
+
+
+	public void setPrice(int price) {
+		this.price = price;
+	}
+
+
+	public boolean isHotel() {
+		return hotel;
+	}
+
+
+	public void setHotel(boolean hotel) {
+		this.hotel = hotel;
+	}
+
+
+	public boolean isBuilding() {
+		return building;
+	}
+
+
+	public void setBuilding(boolean building) {
+		this.building = building;
+	}
+
+
+	public boolean isHouse() {
+		return house;
+	}
+
+
+	public void setHouse(boolean house) {
+		this.house = house;
+	}
 	
-	
-	
+
 }
